1699153b85c6a0a813370f3a3e37e1cd0ac6cd11
natalia <121969070+natrand@users.noreply.github.com>
2023-04-13 10:49:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 int main(){
 
     char symbol[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ()[]{}!@#$%?*-_";
-    int password_length = 8;
+    int password_length;
     int str_length = sizeof(symbol)-1;
 
     srand(time(NULL));
-    std::cout<<"Pasword Generator: ";
+    std::cout<<"Enter the password length: ";
+    std::cin>>password_length;
+    std::cout<<"\nPasword Generator: ";
     for(int i=0; i<password_length; i++){
         std::cout<<symbol[rand() % str_length];
     }
